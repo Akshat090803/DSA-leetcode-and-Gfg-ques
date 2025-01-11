54e522d01d0f51df19b3f38a39d2d36c01e1e6f4
Akshat Jain <akshatjain70233@gmail.com>
2025-01-11 15:47:05 +0530

    Added apprach-2 for the problem

--- Difficulty: Easy/LCA in BST/lca-in-bst.cpp
+++ Difficulty: Easy/LCA in BST/lca-in-bst.cpp
@@ -1,22 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
-    int data;
-    Node *left;
-    Node *right;
-
-    Node(int val) {
-        data = val;
-        left = right = NULL;
-    }
-};
-// Function to Build Tree
-
-
-// } Driver Code Ends
-//Function to find the lowest common ancestor in a BST.
 class Solution{
     public:
     
@@ -36,120 +17,53 @@ class Solution{
              path(root->right,target,ans,reached);
          }
      }
+     
+     
         Node* LCA(Node *root, int n1, int n2)
         {
             // code here
             //!Way1
             //store path to n1 and n2 in differnet arrays
             //?than traverse both arr and ele before first mismatched value is the lowestCommon ANcestor
-            bool reached=false;
-            vector<Node*>n1Path;
-            vector<Node*>n2Path;
+            // bool reached=false;
+            // vector<Node*>n1Path;
+            // vector<Node*>n2Path;
             
-            //find path of n1
-            path(root,n1,n1Path,reached);
+            // //find path of n1
+            // path(root,n1,n1Path,reached);
             
-            //find path of n2
-            reached=false;
-            path(root,n2,n2Path,reached);
-            Node* anc;
-            for(int i=0; i<n1Path.size() && i<n2Path.size();i++){
+            // //find path of n2
+            // reached=false;
+            // path(root,n2,n2Path,reached);
+            // Node* anc;
+            // for(int i=0; i<n1Path.size() && i<n2Path.size();i++){
                 
-                if(n1Path[i]->data!=n2Path[i]->data) break;
+            //     if(n1Path[i]!=n2Path[i]) break;
                 
-                anc=n1Path[i]; //we can take n2Path[i] also as ancestor are smae
-            }
-            return anc;
+            //     anc=n1Path[i]; //we can take n2Path[i] also as ancestor are smae
+            // }
+            // return anc;
             
-            
+            //! Way-2 without use of Array
+            // three case
+            //both go right
+            //both go left
+            //diverge
+        
+           if(!root) return nullptr;
+           
+           //both go left
+           if(n1<root->data && n2<root->data){
+               return LCA(root->left,n1,n2);
+           }
+           else if(n1>root->data && n2>root->data){//both go right
+               return LCA(root->right,n1,n2);
+           }
+           else{//when they diverge
+               //when they diverge , the root from which they diverge will be the lowest common ancestor
+               //so return root
+               return root;
+           }
         }
 
 };
-
-
-//{ Driver Code Starts.
-
-Node* buildTree(string str)
-{   
-    // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
-    // string after spliting by space
-    vector<string> ip;
-    
-    istringstream iss(str);
-    for(string str; iss >> str; )
-        ip.push_back(str);
-        
-    // Create the root of the tree
-    Node* root = new Node(stoi(ip[0]));
-        
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-        
-    // Starting from the second element
-    int i = 1;
-    while(!queue.empty() && i < ip.size()) {
-            
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-            
-        // Get the current node's value from the string
-        string currVal = ip[i];
-            
-        // If the left child is not null
-        if(currVal != "N") {
-                
-            // Create the left child for the current node
-            currNode->left = new Node(stoi(currVal));
-                
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-            
-        // For the right child
-        i++;
-        if(i >= ip.size())
-            break;
-        currVal = ip[i];
-            
-        // If the right child is not null
-        if(currVal != "N") {
-                
-            // Create the right child for the current node
-            currNode->right = new Node(stoi(currVal));
-                
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-    
-    return root;
-}
-
-
-int main()
-{
-
-    int t;
-	scanf("%d ",&t);
-    while(t--)
-    {
-        string s;
-        int l , h;
-		getline(cin,s);
-		scanf("%d ",&l);
-		scanf("%d ",&h);
-        Node* root = buildTree(s);
-        Solution sol;
-        int ans = sol.LCA(root, l, h)->data;
-        cout<<ans<<endl;
-    }
-    return 1;
-}
-// } Driver Code Ends
