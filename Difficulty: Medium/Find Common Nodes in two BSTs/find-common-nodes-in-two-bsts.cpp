cfd864110e0a3bfca891b39752fb869028b8611b
Akshat Jain <akshatjain70233@gmail.com>
2025-01-11 19:10:03 +0530

    Update find-common-nodes-in-two-bsts.cpp

--- Difficulty: Medium/Find Common Nodes in two BSTs/find-common-nodes-in-two-bsts.cpp
+++ Difficulty: Medium/Find Common Nodes in two BSTs/find-common-nodes-in-two-bsts.cpp
@@ -1,86 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-// Tree Node
-struct Node {
-    int data;
-    Node* left;
-    Node* right;
-
-    Node(int val) {
-        data = val;
-        left = right = NULL;
-    }
-};
-
-// Function to Build Tree
-Node* buildTree(string str) {
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;)
-        ip.push_back(str);
-
-    // Create the root of the tree
-    Node* root = new Node(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-
-        // Get the current node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N") {
-
-            // Create the left child for the current node
-            currNode->left = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size())
-            break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N") {
-
-            // Create the right child for the current node
-            currNode->right = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
-
-
-// } Driver Code Ends
-
-
 class Solution {
   public:
     // Function to find the nodes that are common in both BST.
@@ -96,8 +13,7 @@ class Solution {
         //we need Common Node in sorted order
         //find inorder traversal of first tree and second tree and store in arr
         //compare value of both arr and push common nodess in final arr
-        //as it is possible that in one tree common node is at different position and in another tree ,common node at different pos
-        //so we us map also and push ele of one traversal in map 
+        //as we have inorder traversal node in final arr will be in sorted order
         
         vector<int>arr1;
         vector<int>arr2;
@@ -105,48 +21,25 @@ class Solution {
         Inorder(r1,arr1);
         Inorder(r2,arr2);
         
-        //map
-        unordered_map<int,bool>present;
-        for(int x:arr1) present[x]=1;
-        
         vector<int>ans;
-        for(x:arr2){
-            if(present[x]){
-                ans.push_back(x);
+        int i=0 , j=0;
+        
+        while(i<arr1.size() && j<arr2.size()){
+            
+            if(arr1[i]<arr2[j]){
+                i++; //we can find equal ele in arr1 later 
+            }
+            else if(arr2[j]<arr1[i]){
+                j++;//we can find equal ele in arr2 later 
+            }
+            else{//when both equal means common
+                ans.push_back(arr2[j]); // we have find
+                i++;
+                j++;
             }
         }
         
         return ans;
         
-        
-        
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    getchar();
-    while (t--) {
-        string s;
-        getline(cin, s);
-        Node* root1 = buildTree(s);
-
-        getline(cin, s);
-        Node* root2 = buildTree(s);
-        Solution ob;
-        vector<int> res = ob.findCommon(root1, root2);
-        for (int i : res)
-            cout << i << " ";
-        cout << endl;
-
-        cout << "~"
-             << "\n";
-    }
-
-    return 1;
-}
-// } Driver Code Ends
