7fdb2f3fba28aaed344d8f542f5477924fdef0b8
Akshat Jain <akshatjain70233@gmail.com>
2025-02-19 22:53:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (56.44%) - LeetSync

---
+++ 394-decode-string/decode-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<pair<string, int>> st; // Stack to store string and repeat count
+        string current_string = "";
+        int num = 0;
+
+        for (char c : s) {
+            if (isdigit(c)) {
+                num = num * 10 + (c - '0'); // Handle multi-digit numbers       //for eg digit is 112 so it will be num=1 than num=11 than num=112
+            } else if (c == '[') {
+                st.push({current_string, num}); // Push current string and count
+                current_string = "";
+                num = 0;
+            } else if (c == ']') {
+                string repeated_string = "";
+                for (int i = 0; i < st.top().second; ++i) {
+                    repeated_string += current_string;
+                }
+                current_string =
+                    st.top().first +
+                    repeated_string; // Combine with previous string
+                st.pop();
+            } else {
+                current_string += c; // Append character to current string
+            }
+        }
+
+        return current_string;
+    }
+};
