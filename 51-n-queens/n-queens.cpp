5ad9950fe51b2c3e4f2c51763d7fc1d145dfec42
Akshat Jain <akshatjain70233@gmail.com>
2025-03-09 16:32:26 +0530

    Time: 3 ms (64.61%) | Memory: 10.3 MB (55.81%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,94 @@
+class Solution {
+public:
+     bool checkDiagonal(int row,int col,int n,vector<string>&Board){
+        int i=row;
+        int j=col;
+        //for checking left upper diagonel
+        //col will decrese and row also decrease
+        while(i>-1 && j>-1){
+            if(Board[i][j]=='Q') return 0; //queen already present in diagonal    
+            i--;
+            j--;
+        }
+
+        i=row; j=col;
+        //for checking Right upper diagonel
+        //col will increase and row  decrease
+        while(i>-1 && j<n){
+            if(Board[i][j]=='Q') return 0; //queen already present in diagonal
+            j++;//col increase
+            i--;
+        }
+
+        return 1; //else true no quuen in diagonal
+
+     }
+
+     void find(int row,int n,vector<vector<string>>&ans,vector<string>&Board,vector<bool>&column){
+
+     //!base condition
+    //  suppose for n=4 so 4 queen will place at row 0 , 1 , 2 and 3
+     //now if we not able to place queen in any of above row , we return back from that row
+     //but if we reach row==n measn we have successfully place all queens in respective row
+     //thats why base condition if row===n (menas queen placed in all rows) than we push board in ans and return
+     if(row==n){
+        ans.push_back(Board);
+        return;
+     }
+
+
+     for(int col=0;col<n;col++){
+
+        if( column[col]==0 && checkDiagonal(row,col,n,Board)){
+            //?if all condtion met for placing queen
+            //place queen
+            Board[row][col]='Q';
+            //mark column reserved for that quuen
+            column[col]=1;
+            //make call for next row queen
+            find(row+1 , n , ans, Board, column);
+
+            //?during backtracking
+            //remove queen form place
+            Board[row][col]='.';
+             // unmark column reserved for that quuen
+            column[col]=0;
+
+        }
+     }
+     }
+    vector<vector<string>> solveNQueens(int n) {
+
+        //!time -> O(N!) 
+        //as for row 1 we check for N col , row 2 we check for N-1 col, row 3 we check for N-2 col
+        //so N*(N-1)*(N-2)*.....*1 => N!
+        //!Space - O( N*N ) due to  vector<string>Board(n); 1-D arr of str consider as 2d arr of char
+
+        //in way-2 for checking for diagonals still the overall complexity will still be same  //!time -> O(N!) 
+        //but we reduced the time for checking diagonal safety to O(1)
+        //so code will optimised little bit
+        
+        vector<vector<string>>ans; //which we will return
+
+        vector<string>Board(n);
+        //arr of string can be visualize as 2d arr of char
+        //initializing borad wit "." and repalce "." with "Q" where Q can be fit
+        for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+        Board[i].push_back('.');
+
+        //this can be done like this Board[i]="...." //suppose for n=4
+        //means we first genrate string af length n and than Board[i]=that_str
+
+        vector<bool>column(n,0); //to ensure in each col only one queen present
+        //!This below two vector will use in way-2 where instead of using check() func for diagonal we will use this two arrays
+        vector<bool>leftDiagonal(n,0);
+        vector<bool>rightDiagonal(n,0);
+        
+        int row=0;//start form row=0
+        find(row,n,ans,Board,column);
+
+        return ans;
+
+    }
+};
