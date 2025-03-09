3afcbc327d55ff8f2da9b5d58c8ba7acd99d319e
Akshat Jain <akshatjain70233@gmail.com>
2025-03-09 16:42:23 +0530

    Time: 3 ms (64.61%) | Memory: 10.1 MB (76.08%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,30 +1,69 @@
 class Solution {
 public:
-     bool checkDiagonal(int row,int col,int n,vector<string>&Board){
-        int i=row;
-        int j=col;
-        //for checking left upper diagonel
-        //col will decrese and row also decrease
-        while(i>-1 && j>-1){
-            if(Board[i][j]=='Q') return 0; //queen already present in diagonal    
-            i--;
-            j--;
-        }
+//!This checkDiagonal func used in way-1
+    //  bool checkDiagonal(int row,int col,int n,vector<string>&Board){
+    //     int i=row;
+    //     int j=col;
+    //     //for checking left upper diagonel
+    //     //col will decrese and row also decrease
+    //     while(i>-1 && j>-1){
+    //         if(Board[i][j]=='Q') return 0; //queen already present in diagonal    
+    //         i--;
+    //         j--;
+    //     }
 
-        i=row; j=col;
-        //for checking Right upper diagonel
-        //col will increase and row  decrease
-        while(i>-1 && j<n){
-            if(Board[i][j]=='Q') return 0; //queen already present in diagonal
-            j++;//col increase
-            i--;
-        }
+    //     i=row; j=col;
+    //     //for checking Right upper diagonel
+    //     //col will increase and row  decrease
+    //     while(i>-1 && j<n){
+    //         if(Board[i][j]=='Q') return 0; //queen already present in diagonal
+    //         j++;//col increase
+    //         i--;
+    //     }
 
-        return 1; //else true no quuen in diagonal
+    //     return 1; //else true no quuen in diagonal
 
-     }
+    //  }
 
-     void find(int row,int n,vector<vector<string>>&ans,vector<string>&Board,vector<bool>&column){
+//!way-1 where we use checkDiagonal func for diagonal safety
+    //  void find(int row,int n,vector<vector<string>>&ans,vector<string>&Board,vector<bool>&column){
+
+    //  //!base condition
+    // //  suppose for n=4 so 4 queen will place at row 0 , 1 , 2 and 3
+    //  //now if we not able to place queen in any of above row , we return back from that row
+    //  //but if we reach row==n measn we have successfully place all queens in respective row
+    //  //thats why base condition if row===n (menas queen placed in all rows) than we push board in ans and return
+    //  if(row==n){
+    //     ans.push_back(Board);
+    //     return;
+    //  }
+
+
+    //  for(int col=0;col<n;col++){
+         
+    //      //!for way-1 where we use checkDiagonal Func for diagonal checking
+    //     if( column[col]==0 && checkDiagonal(row,col,n,Board)){
+    //         //?if all condtion met for placing queen
+    //         //place queen
+    //         Board[row][col]='Q';
+    //         //mark column reserved for that quuen
+    //         column[col]=1;
+    //         //make call for next row queen
+    //         find(row+1 , n , ans, Board, column);
+
+    //         //?during backtracking
+    //         //remove queen form place
+    //         Board[row][col]='.';
+    //          // unmark column reserved for that quuen
+    //         column[col]=0;
+
+    //     }
+    //  }
+    //  }
+
+//!way-2 we not use check_diagonal func 
+//but still the overall time complexity will be same to way-1
+     void find_2(int row,int n,vector<vector<string>>&ans,vector<string>&Board,vector<bool>&column,vector<bool>&leftDiagonal,vector<bool>&rightDiagonal){
 
      //!base condition
     //  suppose for n=4 so 4 queen will place at row 0 , 1 , 2 and 3
@@ -38,21 +77,34 @@ public:
 
 
      for(int col=0;col<n;col++){
+         
+        int ind_for_leftDiag_Arr=(n-1)+col-row;
+        int ind_for_rightDiag_Arr=row+col;
+        //see .tldr file for why?
 
-        if( column[col]==0 && checkDiagonal(row,col,n,Board)){
+        if( column[col]==0 && leftDiagonal[ind_for_leftDiag_Arr]==0 && rightDiagonal[ind_for_rightDiag_Arr]==0){
             //?if all condtion met for placing queen
             //place queen
             Board[row][col]='Q';
             //mark column reserved for that quuen
             column[col]=1;
+            //mark left diagonal reserved for queen
+            leftDiagonal[ind_for_leftDiag_Arr]=1 ;
+            //mark right diagonal reserved
+             rightDiagonal[ind_for_rightDiag_Arr]=1;
+
             //make call for next row queen
-            find(row+1 , n , ans, Board, column);
+            find_2(row+1 , n , ans, Board, column,leftDiagonal,rightDiagonal);
 
             //?during backtracking
             //remove queen form place
             Board[row][col]='.';
              // unmark column reserved for that quuen
             column[col]=0;
+             // unmark left diagonal reserved for queen
+            leftDiagonal[ind_for_leftDiag_Arr]=0;
+            //unmark right diagonal reserved
+             rightDiagonal[ind_for_rightDiag_Arr]=0;
 
         }
      }
@@ -67,7 +119,7 @@ public:
         //in way-2 for checking for diagonals still the overall complexity will still be same  //!time -> O(N!) 
         //but we reduced the time for checking diagonal safety to O(1)
         //so code will optimised little bit
-        
+
         vector<vector<string>>ans; //which we will return
 
         vector<string>Board(n);
@@ -86,7 +138,8 @@ public:
         vector<bool>rightDiagonal(n,0);
         
         int row=0;//start form row=0
-        find(row,n,ans,Board,column);
+        // find(row,n,ans,Board,column);
+          find_2(row , n , ans, Board, column,leftDiagonal,rightDiagonal);
 
         return ans;
 
