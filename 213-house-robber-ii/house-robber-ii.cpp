2fc131792916cbb200ac9998513ad699018f27f4
Akshat Jain <akshatjain70233@gmail.com>
2025-03-20 22:54:34 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (65.80%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -50,15 +50,47 @@ public:
         }
 
         //! Dp Bottom up approach
-        if(n==1)
+        // if(n==1)
+        // return nums[0];
+
+        // if(n==2)
+        // return max(nums[0],nums[1]);
+
+        // int arr1_opt=bottom_up(arr1);
+        // int arr2_opt=bottom_up(arr2);
+
+        // return max(arr1_opt,arr2_opt);
+
+        //!Dp space optimization
+        //?without dp array and arr1 and arr2 are also not used
+        //we will take prev1 and prev2 
+        //and curr1 and curr2 (curr1 for arr1 ) and (curr2 for arr2)
+        //?value initilaization of prev1 and prev2 for arr1 and arr2 will be different
+       if(n==1)
         return nums[0];
 
         if(n==2)
         return max(nums[0],nums[1]);
-        
-        int arr1_opt=bottom_up(arr1);
-        int arr2_opt=bottom_up(arr2);
+         
+         //!for  (ind=0 to ind= n-2 (second last ele))
+        int prev1=max(nums[1],nums[0]);
+        int prev2=nums[0];
+        int curr1=prev1; //(we iniitialse curr1 with prev1 instead of 0 becuse if size of nums is 3 ,cnsider eg: [2,3,2]  )
+        for(int i=2;i<n-1;i++){
+            curr1=max(prev1,nums[i]+prev2);
+            prev2=prev1;
+            prev1=curr1;
+        }
 
-        return max(arr1_opt,arr2_opt);
+         //!for  (ind=1 to ind= n-1 (second ele to  last ele))
+         prev1=max(nums[1],nums[2]);
+         prev2=nums[1];
+         int curr2=prev1;
+           for(int i=3;i<n;i++){
+            curr2=max(prev1,nums[i]+prev2);
+            prev2=prev1;
+            prev1=curr2;
+        }
+   return max(curr1,curr2);
     }
 };
