498babbd5d866cd7b73e255f62398896a93efc70
Akshat Jain <akshatjain70233@gmail.com>
2025-03-20 20:50:34 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (6.05%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+   //!way-1 (recursion)
+   //?we will get T.L.E
+//    int max_way1(int i,vector<int>&nums){
+//     //only one house so we will steal form it
+//     if(i==0)
+//     return nums[0];
+
+// //we go out of bound that is no such house exist so return 0 we steal 0 money
+//     // if(i<0)
+//     // return 0;
+//  //!the above conditon can also return as
+// //if we see recursion tree we see we are getting -ve i when we are at i=1 ; as form i=1 we can steal at i=1 house and skip adjacent and check for i-2 house i.e, 1-2=-1 or we cAN SKip i=1 house and steal from i=0 house 
+// //?so we can write base case for i=1 i.e, return max(nums[0],nums[1]+0) ~ max(nums[0],nums[1])
+//     //the above conditon can also return as
+
+//     if(i==1){
+//         return max(nums[0],nums[1]);
+//     }
+
+//     return max(nums[i]+max_way1(i-2,nums) , max_way1(i-1,nums));
+//    } 
+
+//-----------------------------------------------------------------
+   //!optimise using DP Top Down Approach
+     int dp_TopDown(int i,vector<int>&nums,vector<int>&dp){
+    //only one house so we will steal form it
+    if(i==0)
+    return nums[0];
+
+    if(i==1){
+        return max(nums[0],nums[1]);
+    }
+
+    if(dp[i]!=-1)
+    return dp[i];
+
+    return dp[i]=max(nums[i]+dp_TopDown(i-2,nums,dp) , dp_TopDown(i-1,nums,dp));
+   } 
+
+    int rob(vector<int>& nums) {
+         
+         int i=nums.size()-1;
+         //T.L.E for way-1 
+        //  return max_way1(i,nums);
+
+        //!Dp topdown
+        //?size of dp array
+        //for nums of size 6 ,the max func call will be of i=size-1
+        //so max index for dp arr will be size()-1
+        //so simply make dp array of size nums.szie() as last ind wil be size-1
+        vector<int>dp(nums.size(),-1);
+        return dp_TopDown(i,nums,dp);
+
+    }
+};
