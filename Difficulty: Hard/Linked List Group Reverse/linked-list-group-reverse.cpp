ddfd2fdd5658c20f9c90f1bad881636595e48bee
Akshat Jain <akshatjain70233@gmail.com>
2025-07-15 15:39:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Linked List Group Reverse/linked-list-group-reverse.cpp
+++ Difficulty: Hard/Linked List Group Reverse/linked-list-group-reverse.cpp
@@ -1,32 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
-using namespace std;
-
-struct Node {
-    int data;
-    Node* next;
-
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};
-
-/* Function to print linked list */
-void printList(Node* node) {
-    while (node != NULL) {
-        printf("%d ", node->data);
-        node = node->next;
-    }
-    printf("\n");
-}
-
-
-// } Driver Code Ends
 /*
   Node is defined as
     struct node
@@ -45,81 +16,37 @@ void printList(Node* node) {
 class Solution {
   public:
     Node *reverseKGroup(Node *head, int k) {
-         Node*first=new Node(0);//create dummy node
-     first->next=head;
-     head=first;
-     
-     
-     while(first->next){
-         
-         Node*prev=first;
-         Node*second=first->next;
-         Node*curr=first->next;
-         
-         int x=k;
-         while(x-- && curr!=nullptr){
-             Node*future=curr->next;
-             curr->next=prev;
-             prev=curr;
-             curr=future;
-         }
+        // code here
+        Node* first=new Node(0);
+        first->next=head;
+        head=first;
         
-         first->next=prev;
-         second->next=curr;
+        //first is already pointing to Head
+        Node*second=head->next;
+        Node*prev=head;
+        Node* curr=second;
+        int kc=k;
+        while(curr!=nullptr){
+            prev=first;
+            curr=second;
+            
+            kc=k;
+            while(kc-- && curr!=nullptr){
+                Node*future=curr->next;
+                curr->next=prev;
+                prev=curr;
+                curr=future;
+            }
+            
+            first->next=prev;
+            second->next=curr;
+            first=second;
+            second=curr;
+        }
         
-         first=second;
-          
-     }  
-         
          Node*dummy=head;
-         head=head->next;
-         delete dummy;//deleete dummmy node i.e, first node whose addres is inhead
-         return head;
+        head=head->next;
+        delete dummy;//deleting dummy node
+        return head;
     }
 };
-
-
-//{ Driver Code Starts.
-
-/* Driver program to test above function*/
-int main(void) {
-
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        if (arr.empty()) {
-            cout << -1 << endl;
-            continue;
-        }
-
-        int data = arr[0];
-        Node* head = new Node(data);
-        Node* tail = head;
-        for (int i = 1; i < arr.size(); ++i) {
-            data = arr[i];
-            tail->next = new Node(data);
-            tail = tail->next;
-        }
-        int k;
-        cin >> k;
-        cin.ignore();
-
-        Solution ob;
-        head = ob.reverseKGroup(head, k);
-        printList(head);
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-// } Driver Code Ends
