92584c410a4f210313d5b1bef421469eaa590970
Akshat Jain <akshatjain70233@gmail.com>
2025-07-26 17:58:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (45.21%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.cpp
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq(stones.begin(),stones.end());
 
-        //as we required frquent access to max value
-        //and we will do insertion and deletion both, so we will use MaxHeap
-        priority_queue<int>pq; //MaxHeap
-        for(int x:stones) pq.push(x);
-
-        while(pq.size()>=2){
-            int top_stone=pq.top();
+        while(pq.size()>1){
+            int y= pq.top(); //first largeste
             pq.pop();
-            int blastResult= top_stone - pq.top(); //top stone - second top stone
-            pq.pop();
-
-            if(blastResult) pq.push(blastResult);//don't push when blastResult=0
+            int x=pq.top(); //second largest i.e, x<=y
+             pq.pop();
+            if(y-x){ //this will handle all case if y==x than y-x=0 so condition false , nothing will push , and we know y>=x so y-x always +ve
+                pq.push(y-x);
+            }
         }
 
-        return (pq.size()) ? pq.top() : 0;
-        // At the end of the game, there is at most one stone left.
-        // Return the weight of the last remaining stone. If there are no stones left, return 0.
+        if(pq.empty()) return 0;
+
+        return pq.top();
     }
 };
