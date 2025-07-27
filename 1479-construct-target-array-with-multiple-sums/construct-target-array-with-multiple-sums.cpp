e446e91ddd1fa42385645ae31cf1fd1350685ab0
Akshat Jain <akshatjain70233@gmail.com>
2025-07-28 00:53:44 +0530

    Time: 0 ms (100.00%) | Memory: 32.8 MB (97.80%) - LeetSync

--- 1479-construct-target-array-with-multiple-sums/construct-target-array-with-multiple-sums.cpp
+++ 1479-construct-target-array-with-multiple-sums/construct-target-array-with-multiple-sums.cpp
@@ -1,46 +1,33 @@
 class Solution {
 public:
     bool isPossible(vector<int>& target) {
-        
-        //instead of constructing target array from arr having 1 1 1... initially
-        //we will prcess in backward direction i.e, from target arr and see whether we can get arr having 1 1 1 1 1..
-    //the max ele will be the x (the sum of all elements currently in your array.)
-    //remainng sum = sum - maxEle
-    //ELE= MaxEle % remainnf sum
-    //!As we will need maxzele at each iteration we will use MAxHeap
-   //?we will run while loop until MaxHeap top is not equal to 1 as when top is 1 it means all ele in maxHeap is 1 i.em arr=1,1,1..... starting point as per ques
-    priority_queue<int>pq(target.begin(),target.end()); //MAxHeap
-     
-     long long remainSum=0;
-     long long sum=0;
-     for(auto x:target) sum+=x;
-    
-     while(pq.top()!=1){
-        
-         long long maxEle=pq.top();
-         pq.pop();
-         remainSum=sum-maxEle;
-         if(remainSum<=0 || remainSum>maxEle) return 0;
+        //if we start form [1,1,1,...] , it will be very difficult to judge at which position sum should place so that we can reach target array.
+        ///so we use reverse approach , we start from target array , and try check if we can reach [1,1,1....]
 
-    //   long long Element=maxEle-remainSum;
-    //this will give tle for  target=[10000000, 1] etc.
-    
-    // The % prevents the TLE from the case (e.g., target=[10000000, 1]) where the largest element is significantly bigger than the sum of other elements.
-         long long Element=maxEle%remainSum;
-         if(Element==0){
-            if(remainSum==1) return true;
-            else return false;
-         }
+        //we use max heap , the highest value will be the recent value in which sum was added and pop it.
+        //to get actual value of Max ele , we subtract rest ele sum form the MaxELe as as actualValue + Rest values sum = MaxEle;
+        //if actaul value ==1 dont push again else push it again as it needs to redue further 
+        //for next iteration , sum will be Sum - newMaxTop + Actaul VAlue
+        //?why?  suppsoe [9,3,5] 
+        // 
+        priority_queue<int>pq(target.begin(),target.end());
+        long long sum=0;
+        for(int x:target) sum+=x;
+        sum-=pq.top();
+        while(pq.top()!=1 && !pq.empty()){
+            if(sum <=0 || sum>pq.top()) return false; //consider [1,1,1,2]
+            // int actualValue=pq.top() - sum;
+            int actualValue=pq.top() % sum; //to avaoid overflow [1,10000000] , we use % instead of -
+            pq.pop();
+            if(actualValue > 1) pq.push(actualValue);
+            else if (actualValue==0){ //suppse pq.top() was 2 and sum=1 sp pq.top()%1 i.em 2%1 = 0 , as we are using % , if we used - ,than 2-1 =1 which was required, so if actaulValue 0 and sum is 1  , return true
+                if(sum==1) return true;
+                else return false;
+            }
 
-        // sum=maxEle ; //we are moving backward;
-         //so inprevious iteration(if we see backwardly sum will be maxEle that;s why in current iteration , it is maxEle)
-         //?or we can right this as
-         sum=remainSum+Element;
-         pq.push(Element);
-       
-     }
-
-     return 1;
-     
+            sum-=pq.top();
+            sum+=actualValue;
+        }
+        return true;
     }
 };
