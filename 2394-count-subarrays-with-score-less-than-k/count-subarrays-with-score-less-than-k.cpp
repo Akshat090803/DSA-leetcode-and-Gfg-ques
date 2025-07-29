699e8ece54230150b95416b5a92bbb7283f2727e
Akshat Jain <akshatjain70233@gmail.com>
2025-07-29 21:09:50 +0530

    Time: 4 ms (44.58%) | Memory: 99 MB (41.72%) - LeetSync

--- 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.cpp
+++ 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.cpp
@@ -7,24 +7,26 @@ public:
         //in subarray product less than k we were maintinaing product variaable but here we will maintain sum , score variable
         //jb tk score km h hm window size icrease krnege aur sum+=arr[end]
         //agr score jyada ho jata he to ,window reduce krenge aur sum-=arr[start] aur socre bhi km krnege
-
+        int len=1;
         long long sum=0,score=0;
         long long count=0,start=0,end=0;
 
         while(end<nums.size()){
             sum+=nums[end];
-            score=sum*(1+end-start); // 1+(end-start) will give length of window
+            score=sum*len; // 1+(end-start) will give length of window
 
             while(score>=k && start<=end){
                 sum-=nums[start]; //reduce the sum as arr[start] will remove from window
                 start++;  //start++ to reduce length
                 //calcualted updates score
-                score=sum*(1+end-start); //1+end-start gives new length of window
+                len--;
+                score=sum*len; //1+end-start gives new length of window
                 
             }
 
-            count+=1+(end-start); //size of windpw will tells about how many subarr
+            count+=len; //size of windpw will tells about how many subarr
             end++;
+            len++;
         }
         return count;
     }
