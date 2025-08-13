64222414afd05fe07eb1474faa4d25a2ee7e4812
Akshat Jain <akshatjain70233@gmail.com>
2025-08-13 20:27:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
+++ Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
@@ -1,82 +1,28 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-bool comp(pair<int,int>a,pair<int,int>b){
-    return a.second < b.second;
-    //return true if a is smaller than b
-    //question ask is a and b at correct posiition
-}
-
-
 class Solution {
   public:
     // Function to find the maximum number of meetings that can
     // be performed in a meeting room.
     int maxMeetings(vector<int>& start, vector<int>& end) {
-        //we will take meeting whose end time is small
-        // so we need to sort end time array along with ist respective start time
+        // Your code here
+        vector<pair<int,int>>v; //end time , start time
         
-        vector<pair<int,int>>meeting; // in pair <start,end>
-        for(int i=0;i<start.size();i++)
-        meeting.push_back({start[i],end[i]});
+        for(int i=0;i<start.size();i++){
+        v.push_back({end[i],start[i]});
+        }
         
+        sort(v.begin(),v.end());
         
-        //now sort on basis of end time
+        int count =1;
+        int endTime=v[0].first;
         
-        sort(meeting.begin(),meeting.end() , comp);
-        
-        int total=0;
-        int ending=-1; 
-        for(int i=0;i<meeting.size();i++){
-            pair<int,int>currMeet=meeting[i];
-            //checking we can conduct this meeting
-            //if curr meeting start after the end of previous meeting than we can do curr meeting
-            if(currMeet.first > ending){
-                total++;
-                ending=currMeet.second;
-                ///now the ending will be the end time of curr meeting
+        for(int i=1;i<v.size();i++){
+            if(endTime < v[i].second){
+                count++;
+                endTime=v[i].first;
             }
         }
-    
-        return total;
+        
+        return count;
+        
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        string input;
-        int num;
-        vector<int> start;
-
-        getline(cin, input);
-        stringstream s2(input);
-        while (s2 >> num) {
-            start.push_back(num);
-        }
-
-        vector<int> end;
-        getline(cin, input);
-        stringstream s22(input);
-        while (s22 >> num) {
-            end.push_back(num);
-        }
-
-        Solution ob;
-        int ans = ob.maxMeetings(start, end);
-        cout << ans << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
