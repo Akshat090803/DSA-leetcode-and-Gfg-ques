5ae077fe91057d1f4ee27d550f32972e3663506e
Akshat Jain <akshatjain70233@gmail.com>
2025-08-14 01:04:23 +0530

    Time: 258 ms (15.69%) | Memory: 169.9 MB (11.98%) - LeetSync

--- 1962-single-threaded-cpu/single-threaded-cpu.cpp
+++ 1962-single-threaded-cpu/single-threaded-cpu.cpp
@@ -2,51 +2,51 @@ class Solution {
 public:
     vector<int> getOrder(vector<vector<int>>& tasks) {
         
-        vector<pair<int,pair<int,int>>>process; //<enqueue,{time,name} >
-        for(int i=0;i<tasks.size();i++){
-         process.push_back({tasks[i][0],{tasks[i][1],i}});
-        }
-        
+
         vector<int>ans;
+        vector<vector<int>>v(tasks.size());
 
-        // sort on basis of enque time
-        //so that we can get enque time od processes in ascending order
+        for(int i=0;i<tasks.size();i++){
+               int arr=tasks[i][0];
+               int prr=tasks[i][1];
+               v[i].push_back(arr);
+               v[i].push_back(i);
+               v[i].push_back(prr);
+        }
 
-        sort(process.begin(),process.end());
 
-        //create a minHeap whuch will return minmum Processing time wala Task out of all avaialble tasks at that time
-        //in heap we don't need of enque time,
-        //we need procees time (for comparision) and process name for ans vector
+        sort(v.begin(),v.end());
 
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
 
-        long long timer=process[0].first; //iniitialize timer with enque time of first task that arrived 
-        //suppose if we intilaise timer with 0 but first task to arrive is at 100 , than we are running loop form 0 to 99 unneccessary .So intiiaze timer with the eqnue time of first task
+        long long timer=0;
 
-//run until all tasks not completed
+        int i=0;
+        while(i<tasks.size()){
 
-int track=0;
-        while(ans.size()!=tasks.size()){
-            
-            //pushing in pq
-            while( track<process.size() && process[track].first<=timer){
-                int proccTime=process[track].second.first;
-                int proccName=process[track].second.second;
-                pq.push({proccTime,proccName});
-                track++;
+            while(i<tasks.size() && v[i][0] <= timer ){
+                pq.push({v[i][2],v[i][1]});
+                i++;
             }
 
             if(!pq.empty()){
-                auto executeTask=pq.top();
+                auto tp=pq.top();
                 pq.pop();
-                timer+=executeTask.first;
-                ans.push_back(executeTask.second);
-            }
-            else{
-                timer=process[track].first;
+                timer+=tp.first;
+                ans.push_back(tp.second);
+            }else{
+                timer=v[i][0];
             }
         }
 
+    while(!pq.empty()){
+         auto tp=pq.top();
+                pq.pop();
+                timer+=tp.first;
+                ans.push_back(tp.second);
+    }
         return ans;
+
+
     }
 };
