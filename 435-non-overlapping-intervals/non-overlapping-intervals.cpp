8e0eac62d751768ce83df5f1fad780d24fbc7dc5
Akshat Jain <akshatjain70233@gmail.com>
2025-08-21 17:51:04 +0530

    Time: 2036 ms (5.03%) | Memory: 511 MB (6.16%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,30 +1,21 @@
-bool comp(vector<int>a,vector<int>b){
-    return a[1]<b[1];
+bool comp(vector<int> a ,vector<int>b){
+      return a[1] < b [1];
 }
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        
-        //greedy approac
+      
+          sort (intervals.begin(), intervals.end(),comp);
+//jiska end time sbse km he mtlb wo chalu bhi sbse phle hua hoga 
 
-        //if we try to solve by sorting on baiss of start time,we are not getting correct answers
+          int endT = intervals [0][1];
+          int count=0;
+         for(int i=1;i<intervals.size();i++){
+            if(intervals[i][0] < endT) count++;
+            else endT = intervals[i][1];
+         }
 
-        //so try by soritng on basis of end time
-        sort(intervals.begin(),intervals.end(),comp);
 
-        int remove=0;
-        int endTime=INT_MIN;
-        for(auto x:intervals){
-
-           //when not overlapping mdans endTime of precious is smaller than start time of current ,menas previous one will not overlap in interavl of curr;
-            if(endTime<=x[0]){
-                endTime=x[1];
-            }
-            else{
-                //when overLapping
-                remove++;
-            }
-        }
-        return remove;
+         return count;
     }
 };
