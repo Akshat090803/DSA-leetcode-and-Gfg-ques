913f5ffdd2e1120820ad9b3d7eb08ad5589c8ea6
Akshat Jain <akshatjain70233@gmail.com>
2025-08-23 20:10:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (8.84%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,27 +1,50 @@
 class Solution {
 public:
-      int uniq(int i , int j ,int m , int n, vector<vector<int>>&dp){
-        if(i==m-1 && j==n-1) return 1;
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        int ans=0;
-        //if I can go down (not getting out of bound if i go donw) than go down
-        if( i+1 < m){
-            ans+= uniq(i+1,j,m,n,dp);
+    int uniq(int i, int j, int m, int n, vector<vector<int>>& dp) {
+        if (i == m - 1 && j == n - 1)
+            return 1;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        int ans = 0;
+        // if I can go down (not getting out of bound if i go donw) than go down
+        if (i + 1 < m) {
+            ans += uniq(i + 1, j, m, n, dp);
         }
 
-//if I can go right , (not getting out of bound if I go right) than go right
-        if(j+1 < n){
-            ans+= uniq(i,j+1,m,n,dp);
+        // if I can go right , (not getting out of bound if I go right) than go
+        // right
+        if (j + 1 < n) {
+            ans += uniq(i, j + 1, m, n, dp);
         }
 
-        return dp[i][j]= ans;
-      }
+        return dp[i][j] = ans;
+    }
     int uniquePaths(int m, int n) {
-        //we dont take visited array as as there is no chances of coming back to parent as we move only down and right not top and left i.e, form where we come
+        // we dont take visited array as as there is no chances of coming back
+        // to parent as we move only down and right not top and left i.e, form
+        // where we come
 
         //
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return uniq(0,0,m,n,dp);
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
+        // return uniq(0,0,m,n,dp);
+
+        //! Bottom Up Dp
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        // base case
+        //  at m-1 , n-1 value is 1 ,
+        dp[m - 1][n - 1] = 1;
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (i + 1 < m)
+                    dp[i][j] += dp[i + 1][j];
+                if (j + 1 < n)
+                    dp[i][j] += dp[i][j + 1];
+            }
+        }
+
+        return dp[0][0];
     }
 };
