8ea00db324310648694e8ab9ae1a5df4aed93540
Akshat Jain <akshatjain70233@gmail.com>
2025-08-25 01:11:53 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (41.16%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -6,12 +6,14 @@ int minPath (int i , int j , int m , int n ,vector<vector<int>>& grid,vector<vec
     if(i==m-1 && j==n-1 ) return grid[i][j];
     
     if(dp[i][j]!=-1) return dp[i][j];
-    int min_=INT_MAX;
 
+    int min_=INT_MAX; 
+
+//if we can go down , go down 
     if(i+1 < m ){
         min_ = min(min_ , minPath(i+1 , j , m ,n , grid,dp));
     }
-
+//if we can go right , go right;
     if(j+1 < n){
         min_=min(min_ , minPath(i,j+1 , m , n , grid , dp));
     }
@@ -23,7 +25,30 @@ int minPath (int i , int j , int m , int n ,vector<vector<int>>& grid,vector<vec
         int n = grid[0].size();
         // if(m==1 && n==1) return grid[0][0]; //this will also gets handle in the base case of minPath func (top down dp)
 
-        vector<vector<int>>dp(m , vector<int>(n,-1));
-        return minPath(0,0,m,n,grid , dp);
+        // vector<vector<int>>dp(m , vector<int>(n,-1));
+        // return minPath(0,0,m,n,grid , dp);
+
+        //!Bottom Up
+        if(m==1 && n==1) return grid[0][0];
+         vector<vector<int>>dp(m , vector<int>(n,INT_MAX));
+
+         dp[0][0]= grid[0][0];
+
+         for(int i=0;i<m;i++)
+         for(int j=0;j<n;j++){
+            int min_=INT_MAX;
+
+            if(i>0 && dp[i-1][j]!=INT_MAX){
+                dp[i][j]=min(dp[i][j] , dp[i-1][j]+grid[i][j]);
+            }
+
+            if(j>0 && dp[i][j-1]!=INT_MAX){
+                dp[i][j]=min(dp[i][j] , dp[i][j-1]+grid[i][j]);
+            }
+
+            
+         }
+
+         return dp[m-1][n-1];
     }
 };
