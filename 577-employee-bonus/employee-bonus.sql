e25e32b62bd4f8c26ace342eb27df9535e6e3f6c
Akshat Jain <akshatjain70233@gmail.com>
2025-10-17 21:57:35 +0530

    Time: 283 ms (98.52%) | Memory: 0B (100.00%) - LeetSync

---
+++ 577-employee-bonus/employee-bonus.sql
@@ -0,0 +1,6 @@
+-- Write your PostgreSQL query statement below
+select
+emp.name , bon.bonus from
+employee emp left join bonus bon
+on emp.empId =  bon.empId
+where bon.bonus<1000 or bon.bonus IS NULL;
