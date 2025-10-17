92838a254f4059584552322e1f36b23d9c553f99
Akshat Jain <akshatjain70233@gmail.com>
2025-10-17 20:20:32 +0530

    Time: 207 ms (95.48%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -5,16 +5,16 @@
 
 -- Way-2 self join
 
--- select wL.id from
--- weather wL join weather wR
--- on  wR.id = wL.id - 1
--- where wL.temperature > wR.temperature;
+select wL.id from
+weather wL join weather wR
+on  wR.recordDate = wL.recordDate - 1
+where wL.temperature > wR.temperature;
 
-SELECT
-    today.id
-FROM
-    Weather today
-JOIN
-    Weather yesterday ON today.recordDate = yesterday.recordDate + INTERVAL '1 day'
-WHERE
-    today.temperature > yesterday.temperature;
+-- SELECT
+--     today.id
+-- FROM
+--     Weather today
+-- JOIN
+--     Weather yesterday ON today.recordDate = yesterday.recordDate + INTERVAL '1 day'
+-- WHERE
+--     today.temperature > yesterday.temperature;
