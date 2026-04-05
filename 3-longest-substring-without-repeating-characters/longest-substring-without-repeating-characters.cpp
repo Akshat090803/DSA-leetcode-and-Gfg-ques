c3610e9518e234cef4bc897eebc090d2b27cb522
Akshat Jain <akshatjain70233@gmail.com>
2026-04-05 20:51:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (97.23%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -3,7 +3,7 @@ public:
     int lengthOfLongestSubstring(string s) {
         vector<bool>present(256,false);
         int maxLen=0;
-        int currLen=0;
+
         int i =0;
         int j=0;
 
@@ -11,13 +11,11 @@ public:
             if(!present[s[j]]){
                 present[s[j]]=true;
                 j++;
-                currLen++;
-                maxLen=max(maxLen,currLen);
+                maxLen=max(maxLen,j-i);
             }
 
             while(present[s[j]]){
                 present[s[i]]=false;
-                currLen--;
                 i++;
             }
         }
